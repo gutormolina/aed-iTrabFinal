6e6d94f5c17bedb3df638478c58d74e819d7a64e
BrunaCamily <brunacamilydomingues@gmail.com>
2023-08-18 20:07:31 -0300

    f.matrix_getelem() e f.matrix_setelem : ok

--- src/main.c
+++ src/main.c
@@ -5,24 +5,27 @@
 void imprime(Matrix *ini, int linhas, int colunas);
 Matrix* criaCabecas( int linhas, int colunas);
 void insere(Matrix *ini, int linhas, int colunas, int l, int c, float valor);
-Matrix* matrix_create( int colunas , int linhas );
+Matrix* matrix_create( );
 void matrix_print( Matrix* m );
+float matrix_getelem( Matrix* m, int x, int y );
+int cont_linhas(Matrix *m);
+int cont_colunas(Matrix *m);
+void matrix_setelem( Matrix* m, int x, int y, float elem );
 
 int main(void)
 {
-    int linhas = 0, colunas = 0;
-    printf("Digite a qntd de linhas da matriz:");
-    scanf("%d", &linhas);
-    printf("Digite a qntd de colunas da matriz:");
-    scanf("%d", &colunas);
+//  float f = 0;
+	Matrix *ini;
 
-	Matrix *ini ;
+    ini = matrix_create( );
 
-    ini = matrix_create(colunas, linhas);
+    
+//  f = matrix_getelem(ini, 1, 1);
+//  printf("\n f = %.2f", f);
+    matrix_setelem(ini, 1, 1, 1.23);
+    matrix_setelem(ini, 1, 2, 2.34);
 
-//    imprime(ini, linhas, colunas);
     matrix_print( ini );
-
     free(ini);
 
 }
@@ -148,7 +151,13 @@ void insere(Matrix *ini, int linhas, int colunas, int l, int c, float valor){
     }
 }
 
-Matrix* matrix_create( int colunas , int linhas ){
+Matrix* matrix_create( ){
+    int linhas = 0, colunas = 0;
+    printf("Digite a qntd de linhas da matriz:");
+    scanf("%d", &linhas);
+    printf("Digite a qntd de colunas da matriz:");
+    scanf("%d", &colunas);
+
     int  l=-1, c=-1, leituras = 0;
     float valor=0;
     Matrix *ini;
@@ -182,29 +191,11 @@ return ini;
 
 void matrix_print( Matrix* m ){
     int linhas = 0, colunas = 0;
-    Matrix * aux = m;
-    while (1)   // conta a qntd de linhas da matriz
-    {
-        m = m->below;
-        if(m->column != -1){
-            linhas++;
-        }else{
-            break;
-        }
-    }
-    while (1)   // conta a qntd de colunas da matriz
-    {
-         m = m->right;
-        if(m->line != -1){
-            colunas++;
-        }else{
-            break;
-        }
-    }
+    
+    linhas = cont_linhas(m);
+    colunas = cont_colunas(m);
 
-    m = aux;
     printf("%d %d\n", linhas, colunas);
-
 	while(1){
         m = m->below;
              while(1){
@@ -221,12 +212,101 @@ void matrix_print( Matrix* m ){
 	}
 }
 
-/*
 
-void matrix_destroy( Matrix* m ){
+float matrix_getelem( Matrix* m, int x, int y ){
+    int linhas = 0, colunas = 0;
+    
+    linhas = cont_linhas(m);
+    colunas = cont_colunas(m);
+
+    if( x <= linhas && y <= colunas){
+
+        for(int i = 1; i <= y; i++ )
+            m = m->right;
+
+        for (int i = 0; i <= x; i++)
+        {
+            if(m->line == x && m->column == y){
+                return m->info;
+            }else if (m->below->column == -1){
+                return 0;
+            }
+
+            m = m->below;
+        }
+    }else {
+        printf("\tInvalido!");
+        return 0;
+    }
+    return -1;
+}
+
+void matrix_setelem( Matrix* m, int x, int y, float elem ){
+    Matrix *aux = m;
+    int linhas = 0, colunas = 0;
+    linhas = cont_linhas(m);
+    colunas = cont_colunas(m);
+
+    for(int i = 1; i <= y; i++ )
+        m = m->right;
+    for (int i = 0; i <= x; i++)
+    {
+        if(m->line == x && m->column == y){
+            m->info = elem;
+        }else if (m->below->column == -1){  // celula vazia, malloc em novo elemento
+            insere(aux, linhas, colunas, x, y, elem);
+        }
+        m = m->below;
+    }
 
 }
 
+
+/*
+void matrix_destroy( Matrix* m ){   //    INCOMPLETA
+//                                      fazer testes de memoria e revisar a logica,
+//                                      talvez tambem precise de ponteoro p/ anterior
+
+    Matrix *aux;
+    while(1){   // free em td que não for cabeça
+        m = m->below;
+        while(1){
+            if(m->column != -1 && m->line != -1){
+                aux = m->right;
+                free(m);
+                m = aux;
+            } else if (m->line == -1)
+            {
+               break;
+            }else{
+                m = m->right;
+            }
+        }  
+        if(m->column == -1 && m->line == -1)  
+            break;
+	}
+
+    while (1)   // free nas cabeças das linhas
+    {
+        m = m->below;
+        while (1)
+        {   
+            if(m->column != -1){
+                if(m->line != -1){
+            
+                }else{
+                    break;
+                }
+            }else {
+                break;
+            }
+        }
+        if(m->column == -1 && m->line == -1)  
+            break;
+    }
+    
+    
+}
 
 Matrix* matrix_add( Matrix* m, Matrix* n ){
 
@@ -240,13 +320,37 @@ Matrix* matrix_transpose( Matrix* m ){
 
 }
 
-float matrix_getelem( Matrix* m, int x, int y ){
-
-}
-
-void matrix_setelem( Matrix* m, int x, int y, float elem ){
-
-    
-}
-
 */
+
+
+int cont_linhas(Matrix *m){
+    int linhas = 0;
+    Matrix * aux = m;
+    while (1)   // conta a qntd de linhas da matriz
+    {
+        m = m->below;
+        if(m->column != -1){
+            linhas++;
+        }else{
+            break;
+        }
+    }
+    m = aux;
+    return linhas;
+}
+
+int cont_colunas(Matrix *m){
+    int colunas = 0;
+    Matrix * aux = m;
+    while (1)   // conta a qntd de colunas da matriz
+    {
+         m = m->right;
+        if(m->line != -1){
+            colunas++;
+        }else{
+            break;
+        }
+    }
+     m = aux;
+    return colunas;
+}
