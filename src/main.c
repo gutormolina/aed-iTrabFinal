a2ea8c141f053cd256ccc58dacb1fa7e6a1aa86c
BrunaCamily <brunacamilydomingues@gmail.com>
2023-08-23 13:25:13 -0300

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrix.h"
+#include <time.h>
 
 int main(void)
 {
+    double tempo = 0.0;
+    clock_t begin = clock();
 	Matrix *ini1;
 
     ini1 = matrix_create( );
     matrix_print(ini1);
     
     matrix_destroy(ini1);
+    clock_t end = clock();
+    tempo+= (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("TEMPO: %f", tempo);
 }
